e1c23fc62a6617c395dc2565320fb659728e8f26
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-06 09:32:13 -0500

    unlockMedal function that will use a newgrounds API

--- source/medals/MedalChecker.hx
+++ source/medals/MedalChecker.hx
@@ -2,12 +2,31 @@ package medals;
 
 class MedalChecker
 {
-        public static function checkForMedals() {
-                switch(PlayState.instance.Score)
-                {
-                        default:
-                                // Prevent this from spamming the console
-                                // trace('No medal unlocked');
-                }
-        }
+	public static function checkForMedals()
+	{
+		switch (PlayState.instance.Score)
+		{
+			case 100:
+				unlockMedal(OneHundoName, OneHundoID);
+
+			default:
+				// Prevent this from spamming the console
+				// trace('No medal unlocked');
+		}
+	}
+
+	public static function unlockMedal(medalName:MedalNames, medalID:MedalIDs)
+	{
+		// Newgrounds API will unlock the medal here
+	}
+}
+
+enum abstract MedalNames(String)
+{
+	var OneHundoName = 'OneHundo';
+}
+
+enum abstract MedalIDs(Int)
+{
+	var OneHundoID = 0;
 }
