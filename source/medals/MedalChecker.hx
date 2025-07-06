0611816e4cbb93bea153adf0a8a5288a562a97b9
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-06 09:40:39 -0500

    Text popup with sfx and also reworked assets folder

--- source/medals/MedalChecker.hx
+++ source/medals/MedalChecker.hx
@@ -1,12 +1,16 @@
 package medals;
 
+import flixel.text.FlxText;
+import flixel.tweens.FlxTween;
+
 class MedalChecker
 {
 	public static function checkForMedals()
 	{
 		switch (PlayState.instance.Score)
 		{
-			case 100:
+                        // 2 is just for testing
+			case 2:
 				unlockMedal(OneHundoName, OneHundoID);
 
 			default:
@@ -18,6 +22,28 @@ class MedalChecker
 	public static function unlockMedal(medalName:MedalNames, medalID:MedalIDs)
 	{
 		// Newgrounds API will unlock the medal here
+
+		var medalText:FlxText = new FlxText();
+		medalText.text = 'Unlocked medal "$medalName"';
+                medalText.size = 32;
+		medalText.screenCenter(X);
+		medalText.y = FlxG.height - medalText.height - 32;
+
+		FlxG.sound.play(FileManager.getAssetFile('ui/medals/NGFadeIn.${FileManager.SOUND_EXT}'));
+		PlayState.instance.add(medalText);
+
+		FlxTween.tween(medalText, {alpha: 0.0}, 1, {
+			startDelay: 2,
+			onStart: tween ->
+			{
+				FlxG.sound.play(FileManager.getAssetFile('ui/medals/NGFadeOut.${FileManager.SOUND_EXT}'));
+			},
+			onComplete: tween ->
+			{
+				PlayState.instance.remove(medalText);
+				medalText.destroy();
+			}
+		});
 	}
 }
 
